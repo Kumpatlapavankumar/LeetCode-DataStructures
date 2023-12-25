4be3f868b60b1850fdc932325749acf3954022e9
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2023-12-25 09:40:14 +0530

    Create 342. Power of Four.java

---
+++ 342. Power of Four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while(n%4==0){
+            n=n/4;
+        }
+        return n==1;
+    }
+}
