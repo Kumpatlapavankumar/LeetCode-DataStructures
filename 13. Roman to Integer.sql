a46b03dfaf229c1f975ae73506b0268f9799e16d
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-01-12 20:33:22 +0530

    Create 13. Roman to Integer.sql

---
+++ 13. Roman to Integer.sql
@@ -0,0 +1,21 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character,Integer> m=new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        int start=0;
+        for(int i=0;i<s.length();i++){
+            if(i<s.length()-1 && m.get(s.charAt(i)) < m.get(s.charAt(i+1))){
+                start=start-m.get(s.charAt(i));
+            }else{
+                start=start+m.get(s.charAt(i));
+            }
+        }
+        return start;
+    }
+}
