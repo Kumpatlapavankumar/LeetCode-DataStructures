1239d9d7260b79be50a48deb59a95ae5ea1ebd59
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-03-12 14:00:25 +0530

    Create 1619. Mean of Array After Removing Some Elements.java

---
+++ 1619. Mean of Array After Removing Some Elements.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double trimMean(int[] arr) {
+        double length=arr.length*0.05;
+        Arrays.sort(arr);
+        double sum=0;
+        double j=0;
+        for(int i=(int)length;i<arr.length-length;i++){
+            sum=sum+arr[i];
+            j++;
+        }
+        double m=sum/j;
+        return m;
+    }
+}
