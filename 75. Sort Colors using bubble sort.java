144b279b5e10b2352c7517053df124ec6c1e2a4b
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-03-21 18:56:32 +0530

    Create 75. Sort Colors using bubble sort.java

---
+++ 75. Sort Colors using bubble sort.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.length-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+    }
+}
