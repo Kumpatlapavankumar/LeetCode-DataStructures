3c7991cf800825dcad013bd2f5a8c7773128772d
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-04-16 17:40:29 +0530

    Create 2073. Time Needed to Buy Tickets.java

---
+++ 2073. Time Needed to Buy Tickets.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int total=0;
+        for(int i=0;i<tickets.length;i++){
+            if(i<=k){
+                total=total+Math.min(tickets[i],tickets[k]);
+            }else{
+                total=total+Math.min(tickets[i],tickets[k]-1);
+            }
+        }
+        return total;
+    }
+
+}
