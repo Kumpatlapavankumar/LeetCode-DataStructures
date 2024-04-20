38309b134b30d7bf0111c2fd07f3680c15cee0aa
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-04-20 17:53:17 +0530

    Create 1143. Longest Common Subsequence.java

---
+++ 1143. Longest Common Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n=text1.length();
+        int m=text2.length();
+        int[][] t=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    t[i][j]=0;
+                }else if(text1.charAt(i-1)==text2.charAt(j-1)){
+                        t[i][j]=1+t[i-1][j-1];
+                }else{
+                    t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        return t[n][m];
+    }
+}
