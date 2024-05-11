d4dbff44e24cc1b287c82d1de7a0ac38c945023e
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-05-11 21:06:17 +0530

    Create 110. Balanced Binary Tree in java.java

---
+++ 110. Balanced Binary Tree in java.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+    
+    private int checkHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        
+        // Check if the height difference is greater than 1
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        
+        // Return the height of the current node
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
