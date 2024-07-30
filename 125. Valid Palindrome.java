63abd445b017758ddf90e9f43485bf6783da91f6
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-07-30 22:09:04 +0530

    Update 125. Valid Palindrome.java

--- 125. Valid Palindrome.java
+++ 125. Valid Palindrome.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Convert the string to lowercase
         s = s.toLowerCase();
-        
-        // Remove non-alphanumeric characters
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
             if (Character.isLetterOrDigit(c)) {
                 sb.append(c);
             }
         }
-        
-        // Check if the modified string is a palindrome
         String modified = sb.toString();
         int left = 0, right = modified.length() - 1;
         while (left < right) {
