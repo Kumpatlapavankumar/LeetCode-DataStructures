c6e45f6c850651baa2e637e28c677365cb9d21a9
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-08-25 21:45:18 +0530

    Create 240. Search a 2D Matrix II.java

---
+++ 240. Search a 2D Matrix II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int i=row-1;
+        int j=0;
+        while(i>=0 && j<col){
+            if(matrix[i][j]==target){
+                return true;
+            }else if(matrix[i][j]>target){
+                i--;
+            }else{
+                j++;
+            }
+        }
+        return false;
+    }
+}
