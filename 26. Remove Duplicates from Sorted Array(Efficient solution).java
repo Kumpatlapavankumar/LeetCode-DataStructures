db36a009e8ebac1eeeadd9a45f3ab77681ef65b9
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-12-13 21:52:53 +0530

    Update 26. Remove Duplicates from Sorted Array(Efficient solution).java

--- 26. Remove Duplicates from Sorted Array(Efficient solution).java
+++ 26. Remove Duplicates from Sorted Array(Efficient solution).java
@@ -1,6 +1,8 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int result=1;
+        //donot edit this
+
         for(int i=1;i<nums.length;i++){
             if(nums[i]!=nums[i-1]){
                 nums[result]=nums[i];
