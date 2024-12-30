56f5a47adbf0786642271e01d6d57fbabd6e0d8b
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2024-12-30 20:53:32 +0530

    Create Power of 2.java

---
+++ Power of 2.java
@@ -0,0 +1,11 @@
+class Solution {
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(int n) {
+        // code here
+        for(int i = 0;i<n;i++){
+          if((int)Math.pow(2,i) == n){
+              return true;
+          }
+      }
+      return false;
+    }
