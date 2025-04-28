8af43510203d657a21b81b90e1cc926b1ed922ea
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-04-28 22:37:52 +0530

    Create 2302. Count Subarrays With Score Less Than K.java

---
+++ 2302. Count Subarrays With Score Less Than K.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long countSubarrays(int[] nums, long k) {
+        long count = 0, sum = 0;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum * (right - left + 1) >= k) {
+                sum -= nums[left++];
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
+}
