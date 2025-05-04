85f3594c661e52fdb844931661970414ee2d4219
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-04 23:13:30 +0530

    Create 1128. Number of Equivalent Domino Pairs.java

---
+++ 1128. Number of Equivalent Domino Pairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        Map<String, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int[] d : dominoes) {
+            int a = Math.min(d[0], d[1]);
+            int b = Math.max(d[0], d[1]);
+            String key = a + "," + b;
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        for (int val : map.values()) {
+            count += val * (val - 1) / 2;
+        }
+        return count;
+    }
+}
