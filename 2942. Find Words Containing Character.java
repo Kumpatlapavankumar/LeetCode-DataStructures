7968ba56b0dbed17e2622c4f1d4747a38d460fae
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-05-24 22:25:28 +0530

    Create 2942. Find Words Containing Character.java

---
+++ 2942. Find Words Containing Character.java
@@ -0,0 +1,11 @@
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        ArrayList<Integer>map=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(words[i].indexOf(x)!=-1){
+                map.add(i);
+            }
+        }
+        return map;
+    }
+}
