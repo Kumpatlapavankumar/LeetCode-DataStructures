13ab8cf64045a1ad85626fee01b472a1bc206081
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-06-25 22:33:42 +0530

    Create 2040. Kth Smallest Product of Two Sorted Arrays.java

---
+++ 2040. Kth Smallest Product of Two Sorted Arrays.java
@@ -0,0 +1,43 @@
+class Solution {
+    public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long left = -10000000000L;
+        long right = 10000000000L;
+        
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (countProducts(nums1, nums2, mid) < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return left;
+    }
+    
+    private long countProducts(int[] nums1, int[] nums2, long target) {
+        long count = 0;
+        for (int num1 : nums1) {
+            if (num1 == 0) {
+                if (target >= 0) count += nums2.length;
+                continue;
+            }
+            
+            int low = 0, high = nums2.length;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                long product = (long) num1 * nums2[mid];
+                if (product <= target) {
+                    if (num1 > 0) low = mid + 1;
+                    else high = mid;
+                } else {
+                    if (num1 > 0) high = mid;
+                    else low = mid + 1;
+                }
+            }
+            
+            count += (num1 > 0) ? low : nums2.length - low;
+        }
+        return count;
+    }
+}
