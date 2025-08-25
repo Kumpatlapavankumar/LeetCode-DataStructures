cb70d2527c078b77e49e1766a4aaca5175bbf58e
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-08-25 22:04:42 +0530

    Create 498. Diagonal Traverse.java

---
+++ 498. Diagonal Traverse.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return new int[0];
+
+        int m = matrix.length, n = matrix[0].length;
+        int[] result = new int[m * n];
+        int row = 0, col = 0;
+
+        for (int i = 0; i < m * n; i++) {
+            result[i] = matrix[row][col];
+
+            if ((row + col) % 2 == 0) {
+                if (col == n - 1) row++;
+                else if (row == 0) col++;
+                else { row--; col++; }
+            } else {
+                if (row == m - 1) col++;
+                else if (col == 0) row++;
+                else { row++; col--; }
+            }
+        }
+
+        return result;
+    }
+}
