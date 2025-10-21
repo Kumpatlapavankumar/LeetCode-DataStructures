e4917ed0828400e69440c85b57af6e5cc0ad6923
Kumaptlapavankumar <116331395+Kumpatlapavankumar@users.noreply.github.com>
2025-10-21 22:06:22 +0530

    Update Power of 2.java

--- Power of 2.java
+++ Power of 2.java
@@ -1,5 +1,6 @@
 class Solution {
-    // Function to check if given number n is a power of two.
+    // Function to c
+    //heck if given number n is a power of two.
     public static boolean isPowerofTwo(int n) {
         // code here
         for(int i = 0;i<n;i++){
